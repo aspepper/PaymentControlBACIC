8c073521b0cd5b1dfc32d33314d751cf840ea33f
Alex Pimenta <aspepper@hotmail.com>
2022-03-29 16:52:37 -0300

    Ajuste no .gitignore para ignorar o node_modules

--- BacicServicePayment/application/server.js
+++ BacicServicePayment/application/server.js
@@ -2,8 +2,11 @@ import app from './lib/app';
 import bodyParser from 'body-parser';
 import homeRouter from './routes/home';
 import dashboard from './routes/dashboard';
+import express from 'express';
 import path from 'path';
 
+// app.use('/assets',express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
@@ -11,7 +14,6 @@ app.set('view engine', 'pug');
 app.use("/", homeRouter);
 app.use("/home", homeRouter);
 app.use("/dashboard", dashboard);
-
 
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
