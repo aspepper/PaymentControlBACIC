faccf5d743f79bafb07e3e17e797de5ac1cd2c67
Alex Pimenta <aspepper@hotmail.com>
2022-04-04 14:13:16 -0300

    Implementação do Login

--- BacicServicePayment/application/repository/user.js
+++ BacicServicePayment/application/repository/user.js
@@ -59,8 +59,58 @@ class UserRepository {
         return userFind;
     }
 
+    async getById(userId: number): user {
+
+        var userReturn = new user(); 
+        const userFind = await userModel
+                                .findById(userId)
+                                .then(function(record){
+                                    if (record != null){
+                                        userReturn.id = record.Id;
+                                        userReturn.userName = record.UserName;
+                                        userReturn.password = record.Password;
+                                        userReturn.name = record.Name;
+                                        userReturn.document = record.Document;
+                                        userReturn.email = record.Email;
+                                        userReturn.mobile = record.Mobile;
+                                        return record;
+                                    }
+                                    return null;
+                                });
+
+        return userFind;
+    }
+
     async getAll(): userModel[] {
         return await userModel.findAll();
+    }
+
+    async authentication(username: string, password: string): string {
+        console.log("Passing by authentication.");
+        const record = this.get(username);
+        console.log(record);
+        record.then(_user => {
+            console.log("username");
+            console.log(username);
+            console.log("password");
+            console.log(password);
+            console.log("_user.password")
+            console.log(_user.password)
+            let verifyPass = bcrypt.compareSync(password, _user.password, (err, res) => {
+                if (err) return callback(err);
+                if (!res) return callback(new Error('Invalid password'));
+                
+                const token = jwt.encode({
+                 username: username,
+                 expire: Date.now() + (1000 * 60 * 60) //1 hour
+                }, tokenSecret);
+                
+                callback(null, token);
+               });
+            callback(null, verifyPass);
+        });
+        console.log(verifyPass);
+        return false;
     }
 
 }
